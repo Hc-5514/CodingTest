6687e6d84e4a81ed90ea8ad74c2fc6efd22c474c
HC-5514 <tbd05050@naver.com>
2022-07-10 00:01:34 +0900

    [BOJ] 10172번 : 개

---
+++ Baekjoon/solved.ac/CLASS 1/_10172.java
@@ -0,0 +1,18 @@
+/**
+ * 문제 : 개
+ * @author Hc-5514
+ */
+
+package BAEKJOON.Solved_CLASS.CLASS_1;
+
+public class _10172 {
+
+    public static void main(String[] args) {
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
