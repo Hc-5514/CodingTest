16a46ab4801a3b7d2c983602a9b7da2866d14ab6
HC-5514 <tbd05050@naver.com>
2022-07-09 23:56:31 +0900

    [BOJ] 9498번 : 시험 성적

---
+++ Baekjoon/solved.ac/CLASS 1/_9498.java
@@ -0,0 +1,22 @@
+/**
+ * 문제 : 시험 성적
+ * @author Hc-5514
+ */
+
+package BAEKJOON.Solved_CLASS.CLASS_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _9498 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int score = Integer.parseInt(br.readLine());
+        br.close();
+
+        System.out.println(score >= 90 ? 'A' : (score >= 80 ? 'B' : (score >=70 ? 'C' : (score >= 60 ? 'D' : 'F'))));
+    }
+}
