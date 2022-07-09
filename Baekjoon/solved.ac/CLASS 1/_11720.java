de2857142d360a7999fb9c2be556dc1659ca2cff
HC-5514 <tbd05050@naver.com>
2022-07-10 00:23:55 +0900

    [BOJ] 11720번 : 숫자의 합

---
+++ Baekjoon/solved.ac/CLASS 1/_11720.java
@@ -0,0 +1,26 @@
+/**
+ * 문제 : 숫자의 합
+ * @author Hc-5514
+ */
+
+package BAEKJOON.Solved_CLASS.CLASS_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _11720 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            sum += Character.getNumericValue(str.charAt(i));
+        }
+        System.out.println(sum);
+    }
+}
