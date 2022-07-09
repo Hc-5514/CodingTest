584d70ae5a5873d60882d7bdde5474c138f7236d
HC-5514 <tbd05050@naver.com>
2022-07-10 00:23:13 +0900

    [BOJ] 11654번 : 아스키코드

---
+++ Baekjoon/solved.ac/CLASS 1/_11654.java
@@ -0,0 +1,19 @@
+/**
+ * 문제 : 아스키코드
+ * @author Hc-5514
+ */
+
+package BAEKJOON.Solved_CLASS.CLASS_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _11654 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().charAt(0) - 0);
+    }
+}
