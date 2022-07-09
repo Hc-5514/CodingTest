31d8a6bb648eb23ea6932252983733a169fc19cb
HC-5514 <tbd05050@naver.com>
2022-07-09 23:55:47 +0900

    [BOJ] 8958번 : OX퀴즈

---
+++ Baekjoon/solved.ac/CLASS 1/_8958.java
@@ -0,0 +1,33 @@
+/**
+ * 문제 : OX퀴즈
+ * @author Hc-5514
+ */
+
+package BAEKJOON.Solved_CLASS.CLASS_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _8958 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<n; i++){
+            String str = br.readLine();
+            int sum = 0;
+            int count = 0;
+            for(int j=0; j<str.length(); j++){
+                if(str.charAt(j) == 'O'){
+                    count++;
+                    sum += count;
+                }
+                else count = 0;
+            }
+            System.out.println(sum);
+        }
+    }
+}
