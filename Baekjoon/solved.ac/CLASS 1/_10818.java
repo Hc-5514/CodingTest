3b7ba72f5c804ea2e2c41defe1ee163368dd3120
HC-5514 <tbd05050@naver.com>
2022-07-10 00:03:08 +0900

    [BOJ] 10818번 : 최소, 최대

---
+++ Baekjoon/solved.ac/CLASS 1/_10818.java
@@ -0,0 +1,36 @@
+/**
+ * 문제 : 최소, 최대
+ * @author Hc-5514
+ */
+
+package BAEKJOON.Solved_CLASS.CLASS_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _10818 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        br.close();
+
+        StringTokenizer token = new StringTokenizer(str, " ");
+        int max = Integer.parseInt(token.nextToken());
+        int min = max;
+
+        for(int i=1; i<n; i++){
+            int num = Integer.parseInt(token.nextToken());
+            if(num > max)
+                max = num;
+            else if (min > num)
+                min = num;
+        }
+
+        System.out.println(min + " " + max);
+    }
+}
