5d361a22748a79be2463302712d51b480fbbb71f
HC-5514 <tbd05050@naver.com>
2022-07-09 23:57:02 +0900

    [BOJ] 10171번 : 고양이

---
+++ Baekjoon/solved.ac/CLASS 1/_10171.java
@@ -0,0 +1,17 @@
+/**
+ * 문제 : 고양이
+ * @author Hc-5514
+ */
+
+package BAEKJOON.Solved_CLASS.CLASS_1;
+
+public class _10171 {
+
+    public static void main(String[] args) {
+
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
