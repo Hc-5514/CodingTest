10f24d4c085f40ec7893afda87d367b67fcd70f0
HC-5514 <tbd05050@naver.com>
2022-07-09 23:47:14 +0900

    [BOJ] 2739번 : 구구단

---
+++ Baekjoon/solved.ac/CLASS 1/_2739.java
@@ -0,0 +1,23 @@
+/**
+ * 문제 : 구구단
+ * @author Hc-5514
+ */
+
+package BAEKJOON.Solved_CLASS.CLASS_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _2739 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<10; i++){
+            System.out.println(n + " * " + i + " = " + (n*i));
+        }
+    }
+}
