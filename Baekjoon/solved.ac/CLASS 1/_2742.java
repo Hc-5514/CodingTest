e3f672172b5a7896a03729b48ee57432db2b6204
HC-5514 <tbd05050@naver.com>
2022-07-09 23:49:35 +0900

    [BOJ] 2742번 : 기찍 N

---
+++ Baekjoon/solved.ac/CLASS 1/_2742.java
@@ -0,0 +1,21 @@
+/**
+ * 문제 : 기찍 N
+ * @author Hc-5514
+ */
+
+package BAEKJOON.Solved_CLASS.CLASS_1;
+
+import java.util.Scanner;
+
+public class _2742 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for(int i=num; i>=1; i--){
+            System.out.println(i);
+        }
+    }
+}
