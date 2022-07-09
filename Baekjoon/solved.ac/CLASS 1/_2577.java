6d6a5b382e44e9cd8248091c99837f38180a002f
HC-5514 <tbd05050@naver.com>
2022-07-09 23:45:43 +0900

    [BOJ] 2577번 : 숫자의 개수

---
+++ Baekjoon/solved.ac/CLASS 1/_2577.java
@@ -0,0 +1,29 @@
+/**
+ * 문제 : 숫자의 개수
+ * @author Hc-5514
+ */
+
+package BAEKJOON.Solved_CLASS.CLASS_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _2577 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine());
+        int[] ary = new int[10];
+
+        String str = String.valueOf(num);
+        for(int i=0; i<str.length(); i++){
+            ary[str.charAt(i) - '0']++;
+        }
+
+        for(int i=0; i<ary.length; i++){
+            System.out.println(ary[i]);
+        }
+    }
+}
