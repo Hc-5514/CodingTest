96d55962a176b89ec02350b42b2d35e0aeb45ac8
HC-5514 <tbd05050@naver.com>
2022-07-10 00:16:20 +0900

    [BOJ] 10998번 : AxB

---
+++ Baekjoon/solved.ac/CLASS 1/_10998.java
@@ -0,0 +1,22 @@
+/**
+ * 문제 : AxB
+ * @author Hc-5514
+ */
+
+package BAEKJOON.Solved_CLASS.CLASS_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _10998 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer token = new StringTokenizer(br.readLine(), " ");
+        System.out.println(Integer.parseInt(token.nextToken()) * Integer.parseInt(token.nextToken()));
+    }
+}
