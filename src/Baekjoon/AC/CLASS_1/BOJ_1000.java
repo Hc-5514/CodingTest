0519598b656c946d29618594d2a949758d465795
hc-5514 <tbd05050@naver.com>
2022-11-25 11:35:24 +0900

    [BOJ] 1000번: A+B

---
+++ src/Baekjoon/AC/CLASS_1/BOJ_1000.java
@@ -0,0 +1,20 @@
+/**
+ * 문제 : A+B
+ * 분류 : 사칙연산 (Arithmetic)
+ * @author Hc-5514
+ */
+
+package Baekjoon.AC.CLASS_1;
+
+import java.util.Scanner;
+
+public class BOJ_1000 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+    }
+}
