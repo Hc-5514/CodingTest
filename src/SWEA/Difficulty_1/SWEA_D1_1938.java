80045c5404776e2528e57533468da6493c7aab4b
hc-5514 <tbd05050@naver.com>
2022-11-25 11:16:53 +0900

    [SWEA-D1] 1938번 : 아주 간단한 계산기

---
+++ src/SWEA/Difficulty_1/SWEA_D1_1938.java
@@ -0,0 +1,25 @@
+/**
+ * 문제 : 아주 간단한 계산기
+ *
+ * @author Hc-5514
+ */
+
+package SWEA.Difficulty_1;
+
+import java.util.Scanner;
+
+public class SWEA_D1_1938 {
+
+	public static void main(String[] args) throws Exception {
+
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+	}
+
+}
