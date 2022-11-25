5545bc75352736a40619efbb855d77d28f70168a
hc-5514 <tbd05050@naver.com>
2022-11-25 11:40:18 +0900

    [BOJ] 2557번 : Hello World

---
+++ src/Baekjoon/AC/CLASS_1/BOJ_2557.java
@@ -0,0 +1,14 @@
+/**
+ * 문제 : Hello World
+ * @author Hc-5514
+ */
+
+package Baekjoon.AC.CLASS_1;
+
+public class BOJ_2557 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
