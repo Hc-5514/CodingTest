7eb5c1ca21483a59ae1b876e3a6061e38e7071d9
hc-5514 <tbd05050@naver.com>
2022-11-25 11:24:54 +0900

    [SWEA-D1] 2046번 : 스탬프 찍기

---
+++ src/SWEA/Difficulty_1/SWEA_D1_2046.java
@@ -0,0 +1,25 @@
+/**
+ * 문제 : 스탬프 찍기
+ *
+ * @author Hc-5514
+ */
+
+package SWEA.Difficulty_1;
+
+import java.util.Scanner;
+
+public class SWEA_D1_2046 {
+
+	public static void main(String[] args) throws Exception {
+
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T = sc.nextInt();
+
+		for (int test_case = 1; test_case <= T; test_case++) {
+			System.out.print("#");
+		}
+
+	}
+
+}
