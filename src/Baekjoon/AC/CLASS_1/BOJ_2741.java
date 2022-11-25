428cd45f0a17a49e219e76ea20939548a35e7fb3
hc-5514 <tbd05050@naver.com>
2022-11-25 11:47:06 +0900

    [BOJ] 2741번 : N 찍기

---
+++ src/Baekjoon/AC/CLASS_1/BOJ_2741.java
@@ -0,0 +1,22 @@
+/**
+ * 문제 : N 찍기
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.AC.CLASS_1;
+
+import java.util.Scanner;
+
+public class BOJ_2741 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            System.out.println(i);
+        }
+    }
+}
