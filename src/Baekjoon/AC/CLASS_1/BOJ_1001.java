f3c51d552fb05d10e21c28f8b934cdcb477e9a55
hc-5514 <tbd05050@naver.com>
2022-11-25 11:36:11 +0900

    [BOJ] 1001번: A-B

---
+++ src/Baekjoon/AC/CLASS_1/BOJ_1001.java
@@ -0,0 +1,19 @@
+/**
+ * 문제 : A-B
+ * @author Hc-5514
+ */
+
+package Baekjoon.AC.CLASS_1;
+
+import java.util.Scanner;
+
+public class BOJ_1001 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a-b);
+    }
+}
