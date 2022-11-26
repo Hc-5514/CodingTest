bcbb6bea615ad22a85dfb8a42867b927f88e666a
hc-5514 <tbd05050@naver.com>
2022-11-26 17:53:23 +0900

    [BOJ] 10172번 : 개

---
+++ src/Baekjoon/AC/CLASS_1/BOJ_10172.java
@@ -0,0 +1,19 @@
+/**
+ * 문제 : 개
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.AC.CLASS_1;
+
+public class BOJ_10172 {
+
+    public static void main(String[] args) {
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
