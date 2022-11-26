19495038ad4e26ee78247d71d2104f351f18797c
hc-5514 <tbd05050@naver.com>
2022-11-26 18:17:54 +0900

    [BOJ] 11654번 : 아스키코드

---
+++ src/Baekjoon/AC/CLASS_1/BOJ_11654.java
@@ -0,0 +1,20 @@
+/**
+ * 문제 : 아스키코드
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.AC.CLASS_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_11654 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().charAt(0) - 0);
+    }
+}
