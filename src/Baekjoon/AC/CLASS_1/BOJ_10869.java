a9097037bc6216196ce1b083059a4cd87a4ef251
hc-5514 <tbd05050@naver.com>
2022-11-26 18:11:11 +0900

    [BOJ] 10869번 : 사칙연산

---
+++ src/Baekjoon/AC/CLASS_1/BOJ_10869.java
@@ -0,0 +1,34 @@
+/**
+ * 문제 : 사칙연산
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.AC.CLASS_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_10869 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        br.close();
+
+        StringTokenizer token = new StringTokenizer(str, " ");
+        int a = Integer.parseInt(token.nextToken());
+        int b = Integer.parseInt(token.nextToken());
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
+
+
