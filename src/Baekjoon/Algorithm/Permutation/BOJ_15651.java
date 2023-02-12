39d9a06c0434e3df1c431320d0738a3522ecda7c
hc-5514 <tbd05050@naver.com>
2023-02-12 14:08:18 +0900

    [BOJ] 15651번 : N과 M (3)

---
+++ src/Baekjoon/Algorithm/Permutation/BOJ_15651.java
@@ -0,0 +1,47 @@
+/**
+ * 문제 : N과 M (3)
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.Algorithm.Permutation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_15651 {
+
+    static int N, M;
+    static StringBuilder sb = new StringBuilder();
+    static int[] selectedNum = new int[7];
+    public static void recursion(int cnt) {
+
+        if (cnt == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(selectedNum[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            selectedNum[cnt] = i;
+            recursion(cnt + 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        recursion(0);
+
+        System.out.println(sb.toString());
+        br.close();
+    }
+
+}
