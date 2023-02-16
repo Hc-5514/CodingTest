54305349a1bda15f938c6d3d2729b10eec3b3cc1
Hc-5514 <tbd05050@naver.com>
2023-02-16 15:41:49 +0900

    [BOJ] 16926번 : 배열 돌리기 1

--- src/Baekjoon/Unclassified/BOJ_16926.java
+++ src/Baekjoon/Unclassified/BOJ_16926.java
@@ -13,98 +13,59 @@ import java.util.StringTokenizer;
 
 public class BOJ_16926 {
 
-    static StringBuilder sb = new StringBuilder();
-    static int[][] arr1, arr2;
-    static int N, M, R;
-
-    private static void rotate(int[][] arr1, int rotateCnt) {
-
-        if (rotateCnt == R) {
-            return;
-        }
-
-        // 회전
-        // 배열의 범위를 벗어나거나, 숫자가 있다면 방향 전환
-        int cnt = 0; // 방향전환 숫자 4개가 된다면 내부 회전 반복
-        int dx = 0, dy = 0;
-        int n = 0;
-        arr2 = new int[N][M];
-        while (n < N * M) {
-            // 아래로 채우기
-            if (cnt % 4 == 0) {
-                if (cnt != 0 && dx == cnt / 4 - 1)
-                    dx = dy = cnt / 4;
-                // 다음 칸이 배열 범위 밖이거나 숫자가 있다면 방향 전환
-                if (dx + 1 == N || arr2[dx + 1][dy] != 0) {
-                    cnt++;
-                } else {
-                    arr2[dx + 1][dy] = arr1[dx][dy];
-                    dx++;
-                    n++;
-                }
-            }
-            // 오른쪽으로 채우기
-            else if (cnt % 4 == 1) {
-                // 다음 칸이 배열 범위 밖이거나 숫자가 있다면 방향 전환
-                if (dy + 1 == M || arr2[dx][dy + 1] != 0) {
-                    cnt++;
-                } else {
-                    arr2[dx][dy + 1] = arr1[dx][dy];
-                    dy++;
-                    n++;
-                }
-            }
-            // 위쪽으로 채우기
-            else if (cnt % 4 == 2) {
-                // 다음 칸이 배열 범위 밖이거나 숫자가 있다면 방향 전환
-                if (dx - 1 == -1 || arr2[dx - 1][dy] != 0) {
-                    cnt++;
-                } else {
-                    arr2[dx - 1][dy] = arr1[dx][dy];
-                    dx--;
-                    n++;
-                }
-            }
-            // 왼쪽으로 채우기
-            else if (cnt % 4 == 3) {
-                // 다음 칸이 배열 범위 밖이거나 숫자가 있다면 방향 전환
-                if (dy - 1 == -1 || arr2[dx][dy - 1] != 0) {
-                    cnt++;
-                } else {
-                    arr2[dx][dy - 1] = arr1[dx][dy];
-                    dy--;
-                    n++;
-                }
-            }
-        }
-
-        // 반복
-        rotate(arr2, rotateCnt + 1);
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        R = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
 
         // 배열 초기값 입력 받기
-        arr1 = new int[N][M];
+        int[][] arr = new int[N][M];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                arr1[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         // 배열 회전 시키기
-        rotate(arr1, 0);
+        int cnt = Math.min(N, M) / 2;
+        // R번 회전
+        for (int i = 0; i < R; i++) {
+            // 라인 화전
+            for (int j = 0; j < cnt; j++) {
+                int tmp = arr[j][j];
+
+                // ←
+                for (int k = j + 1; k < M - j; k++) {
+                    arr[j][k - 1] = arr[j][k];
+                }
+
+                // ↑
+                for (int k = j + 1; k < N - j; k++) {
+                    arr[k - 1][M - 1 - j] = arr[k][M - 1 - j];
+                }
+
+                // →
+                for (int k = M - 2 - j; k >= j; k--) {
+                    arr[N - 1 - j][k + 1] = arr[N - 1 - j][k];
+                }
+
+                // ↓
+                for (int k = N - j - 2; k >= j; k--) {
+                    arr[k + 1][j] = arr[k][j];
+                }
+
+                arr[j + 1][j] = tmp;
+            }
+        }
 
         // 출력
-        for (int i = 0; i < arr2.length; i++) {
-            for (int j = 0; j < arr2[i].length; j++) {
-                sb.append(arr2[i][j]).append(" ");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                sb.append(arr[i][j]).append(" ");
             }
             sb.append("\n");
         }
