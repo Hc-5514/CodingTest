9ca96bb15bd5cf94c7436ad96f576d6eab98c9bd
Hc-5514 <tbd05050@naver.com>
2023-02-19 09:53:03 +0900

    [BOJ] 11382번 : 꼬마 정민

---
+++ src/Baekjoon/Unclassified/BOJ_11382.java
@@ -0,0 +1,24 @@
+/**
+ * 문제 : 꼬마 정민
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.Unclassified;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_11382 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+        System.out.println(a + b + c);
+    }
+}
