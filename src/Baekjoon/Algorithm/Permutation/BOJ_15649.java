546462029335388081de0077acb593e0ec391e0e
Hc-5514 <tbd05050@naver.com>
2023-03-05 23:18:02 +0900

    [BOJ] 15649번 : N과 M (1)

--- src/Baekjoon/Algorithm/Permutation/BOJ_15649.java
+++ src/Baekjoon/Algorithm/Permutation/BOJ_15649.java
@@ -14,38 +14,38 @@ import java.util.StringTokenizer;
 public class BOJ_15649 {
 
     static StringBuilder sb = new StringBuilder();
-    static int[] selectedNum = new int[9];
-    static boolean[] isSelected = new boolean[9];
+    static int N, M;
 
-    public static void recursion(int N, int M, int depth) {
+    static int[] selectNum;
+    static boolean[] isSelected;
 
+    private static void permutation(int depth) {
         if (depth == M) {
-            for(int i=0; i<depth; i++)
-                sb.append(selectedNum[i]).append(" ");
+            for (int i = 0; i < M; i++) {
+                sb.append(selectNum[i]).append(" ");
+            }
             sb.append("\n");
             return;
         }
 
         for (int i = 1; i <= N; i++) {
-            // 이미 선택된 수는 x
             if (isSelected[i]) continue;
-            // 선택된 수 저장
-            selectedNum[depth] = i;
             isSelected[i] = true;
-            recursion(N, M, depth + 1);
+            selectNum[depth] = i;
+            permutation(depth + 1);
             isSelected[i] = false;
         }
     }
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        recursion(N, M, 0);
+        selectNum = new int[M];
+        isSelected = new boolean[N + 1];
+        permutation(0);
 
         System.out.println(sb.toString());
         br.close();
