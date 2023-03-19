765088886d3c3e671221b8ed743d9305af3b39ca
Hc-5514 <tbd05050@naver.com>
2023-03-19 11:51:49 +0900

    [BOJ] 11053번 : 가장 긴 증가하는 부분 수열

---
+++ src/Baekjoon/AC/CLASS_4/BOJ_11053.java
@@ -0,0 +1,51 @@
+/**
+ * 문제 : 가장 긴 증가하는 부분 수열
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.AC.CLASS_4;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * LIS 알고리즘 (Longest Increasing Subsequence)
+ * 기본적으로 DP를 이용한다.
+ * 이 문제는 O(N²) 풀이법을 이용했다.
+ */
+
+public class BOJ_11053 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine().trim());
+        int[] numArr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int maxLen = 0;
+        int[] dp = new int[N];
+        for (int i = 0; i < N; i++) {
+            int maxDP = 0;
+            for (int j = 0; j < i; j++) {
+                if (numArr[j] < numArr[i]) {
+                    if (maxDP < dp[j]) maxDP = dp[j];
+                }
+            }
+            dp[i] = maxDP + 1;
+            if (maxLen < dp[i]) maxLen = dp[i];
+        }
+
+
+        bw.write(maxLen + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
