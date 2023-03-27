1d37da541f85c7807a52cf9df8be4d9d69603f95
Hc-5514 <tbd05050@naver.com>
2023-03-27 11:49:00 +0900

    [BOJ] 25083번 : 새싹

---
+++ src/Baekjoon/Unclassified/BOJ_25083.java
@@ -0,0 +1,19 @@
+/**
+ * 문제 : 새싹
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.Unclassified;
+
+public class BOJ_25083 {
+
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
