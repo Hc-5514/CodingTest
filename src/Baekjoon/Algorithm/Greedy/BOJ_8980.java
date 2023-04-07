11c4f18aa75b83cac49658e9a498c326d43e15bf
Hc-5514 <tbd05050@naver.com>
2023-04-07 17:32:49 +0900

    [BOJ] 8980번 : 택배 (100점/100점)

--- src/Baekjoon/Algorithm/Greedy/BOJ_8980.java
+++ src/Baekjoon/Algorithm/Greedy/BOJ_8980.java
@@ -7,8 +7,8 @@
 package Baekjoon.Algorithm.Greedy;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.Comparator;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class BOJ_8980 {
@@ -23,48 +23,40 @@ public class BOJ_8980 {
         int C = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(br.readLine().trim());
 
-        PriorityQueue<Box> pq = new PriorityQueue<>(new Comparator<Box>() {
-            @Override
-            public int compare(Box o1, Box o2) {
-                if (o1.sub == o2.sub) {
-                    return o1.from - o2.from;
-                } else return o1.sub - o2.sub;
-            }
-        });
+        Box[] delivery = new Box[M];
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken()) - 1;
             int to = Integer.parseInt(st.nextToken()) - 1;
             int weight = Integer.parseInt(st.nextToken());
-            pq.offer(new Box(from, to, weight));
+            delivery[i] = new Box(from, to, weight);
         }
+
+        Arrays.sort(delivery, new Comparator<Box>() {
+            @Override
+            public int compare(Box o1, Box o2) {
+                if (o1.to == o2.to) {
+                    return o2.from - o1.from;
+                } else return o1.to - o2.to;
+            }
+        });
 
         int totalBox = 0;
         int[] truck = new int[N];
-        w1:
-        while (!pq.isEmpty()) {
-            Box box = pq.poll();
-            if (box.sub == 1) {
-                if (box.weight <= C)
-                    truck[box.from] = box.weight;
-                else truck[box.from] = C;
-                totalBox += truck[box.from];
-            } else {
-                int weight = box.weight;
-                for (int i = box.from; i < box.to; i++) {
-                    weight = Math.min(weight, C - truck[i]);
-                    if (weight == 0) continue w1;
-                }
-                if (box.weight <= weight)
-                    weight = box.weight;
-                for (int i = box.from; i < box.to; i++) {
-                    truck[i] += weight;
-                }
-                totalBox += weight;
+        f1:
+        for (int i = 0; i < M; i++) {
+            Box box = delivery[i];
+            int weight = box.weight;
+            for (int j = box.from; j < box.to; j++) {
+                weight = Math.min(weight, C - truck[j]);
+                if (weight == 0) continue f1;
             }
+            for (int j = box.from; j < box.to; j++) {
+                truck[j] += weight;
+            }
+            totalBox += weight;
         }
-
 
         bw.write(totalBox + "\n");
         bw.flush();
@@ -76,13 +68,11 @@ public class BOJ_8980 {
         int from;
         int to;
         int weight;
-        int sub;
 
         public Box(int from, int to, int weight) {
             this.from = from;
             this.to = to;
             this.weight = weight;
-            this.sub = to - from;
         }
     }
 }
