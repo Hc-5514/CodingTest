5bb707d801aeabfba00923db6ba3025167c7d759
Hc-5514 <tbd05050@naver.com>
2023-05-08 01:03:19 +0900

    [BOJ] 5337번 : 웰컴

---
+++ src/Baekjoon/Unclassified/BOJ_5337.java
@@ -0,0 +1,23 @@
+/**
+ * 문제 : 웰컴
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.Unclassified;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class BOJ_5337 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(".  .   .\n");
+        bw.write("|  | _ | _. _ ._ _  _\n");
+        bw.write("|/\\|(/.|(_.(_)[ | )(/.\n");
+        bw.flush();
+        bw.close();
+    }
+}
