ddee6fe5fe2673c0c1e5582821e7b7e525c519ae
Hc-5514 <tbd05050@naver.com>
2023-05-11 17:58:23 +0900

    [BOJ] 5338번 : 마이크로소프트 로고

---
+++ src/Baekjoon/Unclassified/BOJ_5338.java
@@ -0,0 +1,25 @@
+/**
+ * 문제 : 마이크로소프트 로고
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.Unclassified;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class BOJ_5338 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("       _.-;;-._\n");
+        bw.write("'-..-'|   ||   |\n");
+        bw.write("'-..-'|_.-;;-._|\n");
+        bw.write("'-..-'|   ||   |\n");
+        bw.write("'-..-'|_.-''-._|\n");
+        bw.flush();
+        bw.close();
+    }
+
+}
