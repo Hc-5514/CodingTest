f9012c0fcfa680d3a797e00f735aaa8376fa2cd0
Hc-5514 <tbd05050@naver.com>
2023-07-24 12:58:08 +0900

    [Programmers - Exercise] 할인 행사

---
+++ src/Programmers/Exercise/할인_행사.java
@@ -0,0 +1,44 @@
+package Programmers.Exercise;
+
+public class 할인_행사 {
+
+    Map<String, Integer> map = new HashMap<>();
+    Queue<String> q = new ArrayDeque<>();
+
+    private void reset(String[] want) {
+        map.clear();
+        q.clear();
+        for (int i = 0; i < want.length; i++) {
+            map.put(want[i], 0);
+        }
+    }
+
+    private boolean check(String[] want, int[] number) {
+        for (int i = 0; i < want.length; i++) {
+            if (map.get(want[i]) != number[i]) return false;
+        }
+        return true;
+    }
+
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        reset(want);
+        for (int i = 0; i < discount.length; i++) {
+            String fruit = discount[i];
+            if (map.containsKey(fruit)) {
+                map.put(fruit, map.get(fruit) + 1);
+                q.offer(fruit);
+                if (q.size() == 10) {
+                    if (check(want, number)) {
+                        answer++;
+                    }
+                    String tmp = q.poll();
+                    map.put(tmp, map.get(tmp) - 1);
+                }
+            } else {
+                reset(want);
+            }
+        }
+        return answer;
+    }
+}
