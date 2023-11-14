47bba6fb46e1c2e28e937b747c04087b9bd1df9e
Hc-5514 <tbd05050@naver.com>
2023-11-14 17:15:30 +0900

    [BOJ] 10775번 : 공항

--- src/Baekjoon/Algorithm/DisjointSet/BOJ_10775.java
+++ src/Baekjoon/Algorithm/DisjointSet/BOJ_10775.java
@@ -10,36 +10,38 @@ import java.io.*;
 
 public class BOJ_10775 {
 
-    static int[] airport;
+    private static int[] gate;
 
     private static int find(int n) {
-        if (airport[n] == n) return n;
-        return airport[n] = find(airport[n]);
+        if (gate[n] == n) {
+            return n;
+        }
+        return gate[n] = find(gate[n]);
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int G = Integer.parseInt(br.readLine().trim());
-        int P = Integer.parseInt(br.readLine().trim());
+        int G = Integer.parseInt(br.readLine().trim()); // 1 <= G <= 100,000
+        int P = Integer.parseInt(br.readLine().trim()); // 1 <= P <= 100,000
 
-        int[] plain = new int[P];
-        for (int i = 0; i < P; i++) {
-            plain[i] = Integer.parseInt(br.readLine().trim());
-        }
-
-        airport = new int[G + 1];
-        for (int i = 1; i < G + 1; i++) {
-            airport[i] = i;
+        gate = new int[G + 1];
+        for (int i = 0; i <= G; i++) {
+            gate[i] = i;
         }
 
         int cnt = 0;
         for (int i = 0; i < P; i++) {
-            airport[plain[i]] = find(plain[i]);
-            if (airport[plain[i]] == 0) break;
+            int n = Integer.parseInt(br.readLine().trim());
+            gate[n] = find(n);
+
+            if (gate[n] == 0) {
+                break;
+            }
+
+            gate[gate[n]] = find(gate[gate[n] - 1]);
             cnt++;
-            airport[airport[plain[i]]] = find(airport[airport[plain[i]] - 1]);
         }
 
         bw.write(cnt + "\n");
