e34f540db0f4b91dc0d1e48b1336c06f40623fc3
Hc-5514 <tbd05050@naver.com>
2024-01-25 08:40:10 +0900

    [BOJ] 11942번 : 고려대는 사랑입니다

---
+++ src/Baekjoon/Algorithm/Implementation/BOJ_11942.java
@@ -0,0 +1,21 @@
+/**
+ * 문제 : 고려대는 사랑입니다
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.Algorithm.Implementation;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class BOJ_11942 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write("고려대학교\n");
+		bw.flush();
+		bw.close();
+	}
+}
