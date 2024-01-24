bc324104765699ed4ea66013762071251fd52f89
Hc-5514 <tbd05050@naver.com>
2024-01-25 07:53:32 +0900

    [BOJ] 2743번 : 단어 길이 재기

---
+++ src/Baekjoon/Algorithm/String/BOJ_2743.java
@@ -0,0 +1,26 @@
+/**
+ * 문제 : 단어 길이 재기
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.Algorithm.String;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ_2743 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		bw.write(br.readLine().length() + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
