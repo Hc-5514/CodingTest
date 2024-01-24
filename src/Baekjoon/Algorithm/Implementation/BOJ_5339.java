8b03b4a16817ecd5ee0de086299d231788a7e57c
Hc-5514 <tbd05050@naver.com>
2024-01-25 08:48:10 +0900

    [BOJ] 5339번 : 콜센터

---
+++ src/Baekjoon/Algorithm/Implementation/BOJ_5339.java
@@ -0,0 +1,30 @@
+/**
+ * 문제 : 콜센터
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.Algorithm.Implementation;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class BOJ_5339 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write("     /~\\\n"
+			+ "    ( oo|\n"
+			+ "    _\\=/_\n"
+			+ "   /  _  \\\n"
+			+ "  //|/.\\|\\\\\n"
+			+ " ||  \\ /  ||\n"
+			+ "============\n"
+			+ "|          |\n"
+			+ "|          |\n"
+			+ "|          |\n");
+		bw.flush();
+		bw.close();
+	}
+}
