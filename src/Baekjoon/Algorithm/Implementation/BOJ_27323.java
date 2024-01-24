220a250eb84c7a040014a6c066148363ebbfbada
Hc-5514 <tbd05050@naver.com>
2024-01-25 08:49:57 +0900

    [BOJ] 27323번 : 직사각형

---
+++ src/Baekjoon/Algorithm/Implementation/BOJ_27323.java
@@ -0,0 +1,29 @@
+/**
+ * 문제 : 직사각형
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.Algorithm.Implementation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ_27323 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int A = Integer.parseInt(br.readLine().trim());
+		int B = Integer.parseInt(br.readLine().trim());
+
+		bw.write((A * B) + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
