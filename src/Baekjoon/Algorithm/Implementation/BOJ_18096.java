dcd6407f37506b2ab50182d4f84e6246322a143e
Hc-5514 <tbd05050@naver.com>
2024-01-25 22:57:01 +0900

    [BOJ] 18096번 : Арифметическая магия

---
+++ src/Baekjoon/Algorithm/Implementation/BOJ_18096.java
@@ -0,0 +1,28 @@
+/**
+ * 문제 : Арифметическая магия
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.Algorithm.Implementation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ_18096 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		br.readLine();
+
+		bw.write("1\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
