7013464c6796cd0fa29cc800eea2a90d8c58e055
Hc-5514 <tbd05050@naver.com>
2024-01-25 22:51:58 +0900

    [BOJ] 25311번 : UCPC에서 가장 쉬운 문제 번호는?

---
+++ src/Baekjoon/Algorithm/Implementation/BOJ_25311.java
@@ -0,0 +1,28 @@
+/**
+ * 문제 : UCPC에서 가장 쉬운 문제 번호는?
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.Algorithm.Implementation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ_25311 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		br.readLine();
+
+		bw.write("A\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
