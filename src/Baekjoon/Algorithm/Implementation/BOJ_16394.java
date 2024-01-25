d2c8dc73a447a8d207e691d0ca85afe7eb3931be
Hc-5514 <tbd05050@naver.com>
2024-01-25 09:10:26 +0900

    [BOJ] 16394번 : 홍익대학교

---
+++ src/Baekjoon/Algorithm/Implementation/BOJ_16394.java
@@ -0,0 +1,18 @@
+/**
+ * 문제 : 홍익대학교
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.Algorithm.Implementation;
+
+import java.util.Scanner;
+
+public class BOJ_16394 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println(N - 1946);
+	}
+}
