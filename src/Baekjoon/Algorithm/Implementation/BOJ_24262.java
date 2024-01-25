2e208b363796959006a3b48b5514f4d98dc08304
Hc-5514 <tbd05050@naver.com>
2024-01-25 09:04:30 +0900

    [BOJ] 24262번 : 알고리즘 수업 - 알고리즘의 수행 시간 1

---
+++ src/Baekjoon/Algorithm/Implementation/BOJ_24262.java
@@ -0,0 +1,26 @@
+/**
+ * 문제 : 알고리즘 수업 - 알고리즘의 수행 시간 1
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.Algorithm.Implementation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ_24262 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		bw.write(1 + "\n" + 0 + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
