8e231e29a8099280834f7c86ae807b1a4ab757b1
Hc-5514 <tbd05050@naver.com>
2024-01-25 09:18:32 +0900

    [BOJ] 15962번 : 새로운 시작

---
+++ src/Baekjoon/Algorithm/Implementation/BOJ_15962.java
@@ -0,0 +1,21 @@
+/**
+ * 문제 : 새로운 시작
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.Algorithm.Implementation;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class BOJ_15962 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write("파이팅!!\n");
+		bw.flush();
+		bw.close();
+	}
+}
