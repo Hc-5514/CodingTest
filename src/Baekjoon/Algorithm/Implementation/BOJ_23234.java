e76583509ebc5146e985e94b3e36e809b6988e26
Hc-5514 <tbd05050@naver.com>
2024-01-25 20:55:06 +0900

    [BOJ] 23234번 : The World Responds

---
+++ src/Baekjoon/Algorithm/Implementation/BOJ_23234.java
@@ -0,0 +1,21 @@
+/**
+ * 문제 : The World Responds
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.Algorithm.Implementation;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class BOJ_23234 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write("The world says hello!\n");
+		bw.flush();
+		bw.close();
+	}
+}
