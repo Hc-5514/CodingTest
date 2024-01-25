c75150e2d6961777a7d7497f09d4cadde3caa843
Hc-5514 <tbd05050@naver.com>
2024-01-25 23:19:35 +0900

    [BOJ] 24078번 : 余り (Remainder)

---
+++ src/Baekjoon/Algorithm/Implementation/BOJ_24078.java
@@ -0,0 +1,28 @@
+/**
+ * 문제 : 余り (Remainder)
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.Algorithm.Implementation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ_24078 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int X = Integer.parseInt(br.readLine().trim()) % 21;
+
+		bw.write(X + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
