c2ead79c4099bb8a1d4c6dbf51f6bb660c01ecc1
Hc-5514 <tbd05050@naver.com>
2024-01-25 09:15:16 +0900

    [BOJ] 15733번 : 나는 누구인가

---
+++ src/Baekjoon/Algorithm/Implementation/BOJ_15733.java
@@ -0,0 +1,21 @@
+/**
+ * 문제 : 나는 누구인가
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.Algorithm.Implementation;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class BOJ_15733 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write("I'm Sexy");
+		bw.flush();
+		bw.close();
+	}
+}
