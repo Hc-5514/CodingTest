2ea28a157e402b2da2569357b95795b55ad5b242
Hc-5514 <tbd05050@naver.com>
2024-01-26 11:47:05 +0900

    [BOJ] 27959번 : 초코바

---
+++ src/Baekjoon/Algorithm/Implementation/BOJ_27959.java
@@ -0,0 +1,36 @@
+/**
+ * 문제 : 초코바
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.Algorithm.Implementation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ_27959 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		if (N * 100 >= M) {
+			bw.write("Yes\n");
+		} else {
+			bw.write("No\n");
+		}
+
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
