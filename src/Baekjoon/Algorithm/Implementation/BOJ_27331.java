c4ed6fcd4af7c27a1feb299fa32a6d05a2354889
Hc-5514 <tbd05050@naver.com>
2024-01-26 15:17:37 +0900

    [BOJ] 27331번 : 2 桁の整数 (Two-digit Integer)

---
+++ src/Baekjoon/Algorithm/Implementation/BOJ_27331.java
@@ -0,0 +1,30 @@
+/**
+ * 문제 : 2 桁の整数 (Two-digit Integer)
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.Algorithm.Implementation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ_27331 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int A = Integer.parseInt(br.readLine().trim());
+		int B = Integer.parseInt(br.readLine().trim());
+		int result = A * 10 + B;
+
+		bw.write(result + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
