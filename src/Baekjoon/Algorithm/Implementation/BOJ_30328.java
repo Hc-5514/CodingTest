5f9e5bc4d3e15074d684df3cd490199ae9cd8db8
Hc-5514 <tbd05050@naver.com>
2024-01-26 23:28:34 +0900

    [BOJ] 30328번 : Java Warriors

---
+++ src/Baekjoon/Algorithm/Implementation/BOJ_30328.java
@@ -0,0 +1,26 @@
+/**
+ * 문제 : Java Warriors
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.Algorithm.Implementation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ_30328 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		bw.write(Integer.parseInt(br.readLine().trim()) * 4000 + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
