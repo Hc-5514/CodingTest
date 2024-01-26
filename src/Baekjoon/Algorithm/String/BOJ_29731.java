f73cf45249d146aebd13e33bae62d7b5de5abdec
Hc-5514 <tbd05050@naver.com>
2024-01-26 21:58:52 +0900

    [BOJ] 29731번 : 2033년 밈 투표

--- src/Baekjoon/Algorithm/String/BOJ_29731.java
+++ src/Baekjoon/Algorithm/String/BOJ_29731.java
@@ -11,29 +11,30 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
 
 public class BOJ_29731 {
-
-	private static final String[] strings = {"Never gonna give you up", "Never gonna let you down",
-		"Never gonna run around and desert you", "Never gonna make you cry", "Never gonna say goodbye",
-		"Never gonna tell a lie and hurt you", "Never gonna stop"};
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		boolean isFalse = false;
 		int N = Integer.parseInt(br.readLine().trim());
+
+		boolean isFalse = false;
+		Set<String> set = new HashSet<>();
+		set.add("Never gonna give you up");
+		set.add("Never gonna let you down");
+		set.add("Never gonna run around and desert you");
+		set.add("Never gonna make you cry");
+		set.add("Never gonna say goodbye");
+		set.add("Never gonna tell a lie and hurt you");
+		set.add("Never gonna stop");
 
 		for (int i = 0; i < N; i++) {
 			String str = br.readLine();
-			int cnt = 0;
-			for (int j = 0; j < strings.length; j++) {
-				if (str.equals(strings[j])) {
-					cnt++;
-				}
-			}
-			if (cnt == 0) {
+			if (!set.contains(str)) {
 				isFalse = true;
 				break;
 			}
