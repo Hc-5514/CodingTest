95929d74ff8f38f70e839e5a2deb27e63c92610f
Hc-5514 <tbd05050@naver.com>
2024-01-26 12:28:24 +0900

    [BOJ] 26082번 : WARBOY

---
+++ src/Baekjoon/Algorithm/Implementation/BOJ_26082.java
@@ -0,0 +1,33 @@
+/**
+ * 문제 : WARBOY
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.Algorithm.Implementation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ_26082 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		int result = B / A * C * 3;
+
+		bw.write(result + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
