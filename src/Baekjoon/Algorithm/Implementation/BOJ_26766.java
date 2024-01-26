5e8bf57e7d85da9cc7fa11e898c7a74e5254a76f
Hc-5514 <tbd05050@naver.com>
2024-01-26 18:01:08 +0900

    [BOJ] 26766번 : Serca

---
+++ src/Baekjoon/Algorithm/Implementation/BOJ_26766.java
@@ -0,0 +1,38 @@
+/**
+ * 문제 : Serca
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.Algorithm.Implementation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ_26766 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			bw.write(" @@@   @@@ \n"
+				+ "@   @ @   @\n"
+				+ "@    @    @\n"
+				+ "@         @\n"
+				+ " @       @ \n"
+				+ "  @     @  \n"
+				+ "   @   @   \n"
+				+ "    @ @    \n"
+				+ "     @     \n");
+		}
+
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
