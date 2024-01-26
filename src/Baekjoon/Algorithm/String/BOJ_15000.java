b48208669f16243c2be7f0c39af3e766ecc1e587
Hc-5514 <tbd05050@naver.com>
2024-01-26 11:45:33 +0900

    [BOJ] 15000번 : CAPS

---
+++ src/Baekjoon/Algorithm/String/BOJ_15000.java
@@ -0,0 +1,26 @@
+/**
+ * 문제 : CAPS
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.Algorithm.String;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ_15000 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		bw.write(br.readLine().toUpperCase());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
