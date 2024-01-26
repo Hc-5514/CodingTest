0a68a900003542c77b9566207c022b6ab9b7ea3d
Hc-5514 <tbd05050@naver.com>
2024-01-26 19:49:33 +0900

    [BOJ] 30007번 : 라면 공식

---
+++ src/Baekjoon/Algorithm/Implementation/BOJ_30007.java
@@ -0,0 +1,37 @@
+/**
+ * 문제 : 라면 공식
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.Algorithm.Implementation;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ_30007 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine().trim());
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			int X = Integer.parseInt(st.nextToken());
+			int W = A * (X - 1) + B;
+			bw.write(W + "\n");
+		}
+
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
