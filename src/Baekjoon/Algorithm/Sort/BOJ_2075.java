5cbea1f56a9f1b88e9d03882acc20c298de0510a
Hc-5514 <tbd05050@naver.com>
2024-02-06 16:24:56 +0900

    [BOJ] 2075번 : N번째 큰 수

---
+++ src/Baekjoon/Algorithm/Sort/BOJ_2075.java
@@ -0,0 +1,43 @@
+/**
+ * 문제 : N번째 큰 수
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.Algorithm.Sort;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BOJ_2075 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine().trim());
+		List<Integer> list = new ArrayList<>(N * N);
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				list.add(Integer.parseInt(st.nextToken()));
+			}
+		}
+
+		Collections.sort(list, ((o1, o2) -> o2 - o1));
+
+		bw.write(list.get(N - 1) + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
