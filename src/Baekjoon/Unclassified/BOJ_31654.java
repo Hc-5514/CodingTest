afbf9e7f1a657df4860c077d21540a71ad619583
Hc-5514 <tbd05050@naver.com>
2024-06-05 23:55:34 +0900

    [BOJ] 31654번 : Adding Trouble

---
+++ src/Baekjoon/Unclassified/BOJ_31654.java
@@ -0,0 +1,34 @@
+/**
+ * 문제 : Adding Trouble
+ *
+ * @author Hc-5514
+ */
+
+package Baekjoon.Unclassified;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ_31654 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+
+		if (A + B == C) {
+			bw.write("correct!\n");
+		} else {
+			bw.write("wrong!\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
