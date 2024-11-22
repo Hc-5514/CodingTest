be08f00e5c5f7cf850aa5f588610bfeb7f624963
Hc-5514 <tbd05050@naver.com>
2024-11-23 06:34:06 +0900

    [BOJ] 1238번 : 파티

--- src/Baekjoon/Algorithm/Dijkstra/BOJ_1238.java
+++ src/Baekjoon/Algorithm/Dijkstra/BOJ_1238.java
@@ -19,23 +19,24 @@ import java.util.StringTokenizer;
 
 public class BOJ_1238 {
 
-	private static int X;
+	private static int n, m, x;
+	private static List<List<Road>> roads, reverseRoads;
 
-	private static int[] dijkstra(List<List<Road>> roads) {
-		int[] distance = new int[roads.size()];
+	private static int[] getDijkstra(List<List<Road>> roads) {
+		int[] distance = new int[n + 1];
 		Arrays.fill(distance, Integer.MAX_VALUE);
-		distance[X] = 0;
+		distance[x] = 0;
 
 		PriorityQueue<Road> pq = new PriorityQueue<>();
-		pq.offer(new Road(X, 0));
+		pq.offer(new Road(x, 0));
 
 		while (!pq.isEmpty()) {
 			Road cur = pq.poll();
-			if (distance[cur.to] < cur.var) {
+			if (cur.weight > distance[cur.to]) {
 				continue;
 			}
 			for (Road road : roads.get(cur.to)) {
-				int cost = road.var + distance[cur.to];
+				int cost = distance[cur.to] + road.weight;
 				if (cost < distance[road.to]) {
 					distance[road.to] = cost;
 					pq.offer(new Road(road.to, distance[road.to]));
@@ -46,58 +47,64 @@ public class BOJ_1238 {
 		return distance;
 	}
 
-	public static void main(String[] args) throws IOException {
+	private static void init() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
 
 		st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken()); // 1 <= N <= 1,000
-		int M = Integer.parseInt(st.nextToken()); // 1 <= M <= 10,000
-		X = Integer.parseInt(st.nextToken()); // 1 <= X <= N
+		n = Integer.parseInt(st.nextToken()); // 학생 수
+		m = Integer.parseInt(st.nextToken()); // 도로 수
+		x = Integer.parseInt(st.nextToken()); // 목표 지점
 
-		List<List<Road>> roads = new ArrayList<>();
-		List<List<Road>> reverse_roads = new ArrayList<>();
-		for (int i = 0; i <= N; i++) {
+		roads = new ArrayList<>();
+		reverseRoads = new ArrayList<>();
+
+		for (int i = 0; i <= n; i++) {
 			roads.add(new ArrayList<>());
-			reverse_roads.add(new ArrayList<>());
+			reverseRoads.add(new ArrayList<>());
 		}
 
-		for (int i = 0; i < M; i++) {
+		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
-			int var = Integer.parseInt(st.nextToken());
-			roads.get(from).add(new Road(to, var));
-			reverse_roads.get(to).add(new Road(from, var));
+			int weight = Integer.parseInt(st.nextToken());
+			roads.get(from).add(new Road(to, weight));
+			reverseRoads.get(to).add(new Road(from, weight));
 		}
 
-		int[] distance = dijkstra(roads);
-		int[] reverse_distance = dijkstra(reverse_roads);
-
-		int max = 0;
-		for (int i = 1; i <= N; i++) {
-			max = Math.max(max, distance[i] + reverse_distance[i]);
-		}
-
-		bw.write(max + "\n");
-		bw.flush();
-		bw.close();
 		br.close();
 	}
 
-	static class Road implements Comparable<Road> {
-		int to;
-		int var;
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		public Road(int to, int var) {
-			this.to = to;
-			this.var = var;
+		init();
+
+		int[] distance = getDijkstra(roads);
+		int[] reverseDistance = getDijkstra(reverseRoads);
+
+		int result = 0;
+		for (int i = 1; i <= n; i++) {
+			result = Math.max(result, distance[i] + reverseDistance[i]);
 		}
 
-		@Override
-		public int compareTo(Road o) {
-			return this.var - o.var;
+		bw.write(result + "\n");
+		bw.flush();
+		bw.close();
+	}
+
+	private static class Road implements Comparable<Road> {
+		int to;
+		int weight;
+
+		public Road(int to, int weight) {
+			this.to = to;
+			this.weight = weight;
+		}
+
+		public int compareTo(Road r) {
+			return this.weight - r.weight;
 		}
 	}
 }
