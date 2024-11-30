6a8d19cea310485f3b30bb1cfa1a6e58a2e2f594
Hc-5514 <tbd05050@naver.com>
2024-11-30 20:38:58 +0900

    [Programmers - Exercise] 인사고과

--- src/Programmers/Exercise/인사고과.java
+++ src/Programmers/Exercise/인사고과.java
@@ -15,44 +15,25 @@ public class 인사고과 {
 	// 어떤 사원이 다른 임의의 사원보다 두 점수가 모두 낮은 경우는 석차 제외
 
 	public int solution(int[][] scores) {
-		int[] find = {scores[0][0], scores[0][1]}; // 완호 점수
+		int[] find = scores[0]; // 완호 점수
 
-		// 근무 태도 (내림차순), 동료 평가 (오름차순)
-		Arrays.sort(scores, (o1, o2) -> {
-			if (o1[0] == o2[0]) {
-				return o1[1] - o2[1];
-			}
-			return o2[0] - o1[0];
-		});
+		Arrays.sort(scores, (o1, o2) -> (o1[0] == o2[0] ? o1[1] - o2[1] : o2[0] - o1[0])); // 근무 태도 (내림차순), 동료 평가 (오름차순)
 
-		int max = scores[0][1];
-
-		for (int i = 1; i < scores.length; i++) {
-			if (scores[i][1] < max) {
+		int max = 0, answer = 1;
+		for (int[] score : scores) {
+			if (score[1] < max) {
 				// 완호일 경우: -1 반환
-				if (scores[i][0] == find[0] && scores[i][1] == find[1]) {
+				if (score[0] == find[0] && score[1] == find[1]) {
 					return -1;
 				}
-				// 석차 제외
-				scores[i][0] = -1;
-				scores[i][1] = -1;
 			} else {
-				max = scores[i][1];
+				max = Math.max(max, score[1]);
+				if (score[0] + score[1] > find[0] + find[1]) {
+					answer++;
+				}
 			}
 		}
 
-		// 점수의 합 (내림 차순)
-		Arrays.sort(scores, (o1, o2) -> (o2[0] + o2[1]) - (o1[0] + o1[1]));
-
-		int rank = 1;
-		for (int i = 0; i < scores.length; i++) {
-			if (scores[i][0] + scores[i][1] > find[0] + find[1]) {
-				rank++;
-			} else {
-				break;
-			}
-		}
-
-		return rank;
+		return answer;
 	}
 }
