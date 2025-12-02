8891f59d47111ed86e737c8265fed7aa0ce77fc3
Hc-5514 <tbd05050@naver.com>
2025-12-02 22:36:45 +0900

    [BOJ] 1152번 : 단어의 개수

--- src/Baekjoon/AC/CLASS_1/BOJ_1152.java
+++ src/Baekjoon/AC/CLASS_1/BOJ_1152.java
@@ -1,23 +1,29 @@
 /**
- * 문제 : 단어의 개수
- *
- * @author Hc-5514
+ * 문제: 단어의 개수
+ * 난이도: 브론즈 2
+ * 메모리: 19876KB, 시간: 168ms
+ * 풀이: 문자열
  */
 
 package Baekjoon.AC.CLASS_1;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class BOJ_1152 {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        sc.close();
-
-        StringTokenizer token = new StringTokenizer(str, " ");
-        System.out.println(token.countTokens());
-    }
+		bw.write(st.countTokens() + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
 }
