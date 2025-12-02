9cde2f6fefb33fc0d8945f79568461d5756a85df
Hc-5514 <tbd05050@naver.com>
2025-12-02 23:03:14 +0900

    [BOJ] 2908번 : 상수

--- src/Baekjoon/AC/CLASS_1/BOJ_2908.java
+++ src/Baekjoon/AC/CLASS_1/BOJ_2908.java
@@ -1,30 +1,44 @@
 /**
- * 문제 : 상수
- * @author Hc-5514
+ * 문제: 단어의 개수
+ * 난이도: 브론즈 2
+ * 메모리: 11604KB, 시간: 68ms
+ * 풀이: 상수
  */
 
 package Baekjoon.AC.CLASS_1;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class BOJ_2908 {
 
-    public static void main(String[] args) throws IOException {
+	public static int convert(String numStr) {
+		int result = 0;
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        br.close();
+		for (int i = numStr.length() - 1; i >= 0; i--) {
+			result += Character.getNumericValue(numStr.charAt(i)) * (int)Math.pow(10, i);
+		}
 
-        StringTokenizer token = new StringTokenizer(str, " ");
-        int a = Integer.parseInt(token.nextToken());
-        int b = Integer.parseInt(token.nextToken());
+		return result;
+	}
 
-        a = Integer.parseInt(new StringBuilder().append(a).reverse().toString());
-        b = Integer.parseInt(new StringBuilder().append(b).reverse().toString());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        System.out.println(Math.max(a,b));
-    }
+		String numStr1 = st.nextToken();
+		String numStr2 = st.nextToken();
+
+		int result = Math.max(convert(numStr1), convert(numStr2));
+
+		bw.write(result + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
 }
