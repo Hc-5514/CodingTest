afa4ef82cf96048af1df3dd6e87533bbc1100c32
Hc-5514 <tbd05050@naver.com>
2025-12-03 14:08:19 +0900

    [BOJ] 1157번 : 단어 공부

--- src/Baekjoon/AC/CLASS_1/BOJ_1157.java
+++ src/Baekjoon/AC/CLASS_1/BOJ_1157.java
@@ -1,43 +1,50 @@
 /**
- * 문제 : 단어 공부
- * @author Hc-5514
+ * 문제: 단어 공부
+ * 난이도: 브론즈 1
+ * 메모리: 20544KB, 시간: 160ms
+ * 풀이: 문자열
  */
 
 package Baekjoon.AC.CLASS_1;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class BOJ_1157 {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        int[] arr = new int[26];
+		String inputString = br.readLine();
+		int[] counts = new int[26];
 
-        // 문자별 개수 카운트
-        for(int i=0; i<str.length(); i++){
-            // 아스키코드 값 (대문자: 65~90, 소문자: 97~122)
-            if(str.charAt(i) >= 65 && str.charAt(i) <= 90)
-                arr[str.charAt(i) - 65]++;
-            else
-                arr[str.charAt(i) - 97]++;
-        }
+		for (int i = 0; i < inputString.length(); i++) {
+			char ch = inputString.charAt(i);
+			if (ch >= 'A' && ch <= 'Z') {
+				counts[ch - 'A']++;
+			} else {
+				counts[ch - 'a']++;
+			}
+		}
 
-        // 가장 많이 사용된 문자 확인, 두 개 이상이라면 ? 저장
-        int max = -1;
-        char ch = '?';
-        for (int i=0; i<26; i++){
-            if(arr[i] > max){
-                max = arr[i];
-                ch = (char)(i + 65);
-            }
-            else if(arr[i] == max){
-                ch = '?';
-            }
-        }
+		char answer = 'A';
+		int cnt = counts[0];
+		for (int i = 1; i < 26; i++) {
+			if (counts[i] > cnt) {
+				answer = (char)('A' + i);
+				cnt = counts[i];
+			} else if (counts[i] == cnt) {
+				answer = '?';
+			}
+		}
 
-        // 문자 출력
-        System.out.println(ch);
-    }
+		bw.write(answer + "");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
 }
