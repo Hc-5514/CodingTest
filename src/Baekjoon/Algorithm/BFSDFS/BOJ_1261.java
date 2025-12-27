f24e0a64d47345d2b46d157585200afc86034a55
Hc-5514 <tbd05050@naver.com>
2025-12-27 18:29:59 +0900

    [BOJ] 1261번 : 알고스팟

--- src/Baekjoon/Algorithm/BFSDFS/BOJ_1261.java
+++ src/Baekjoon/Algorithm/BFSDFS/BOJ_1261.java
@@ -1,7 +1,8 @@
 /**
- * 문제 : 알고스팟
- *
- * @author Hc-5514
+ * 문제: 알고스팟
+ * 난이도: 골드 4
+ * 메모리: 12236KB, 시간: 80ms
+ * 풀이: 0-1 BFS
  */
 
 package Baekjoon.Algorithm.BFSDFS;
@@ -18,6 +19,7 @@ import java.util.StringTokenizer;
 
 public class BOJ_1261 {
 
+	private static final int INF = 1_000_000_000;
 	private static final int[] dRow = {-1, 1, 0, 0}, dCol = {0, 0, -1, 1};
 
 	public static void main(String[] args) throws IOException {
@@ -25,74 +27,69 @@ public class BOJ_1261 {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
 
+		// (0,0) -> (n-1,m-1) 이동 시, 벽을 최소 몇 개 부숴야 하는가?
+
+		// n x m 크기 map 입력
 		st = new StringTokenizer(br.readLine());
-		int M = Integer.parseInt(st.nextToken()); // 1 <= N <= 100
-		int N = Integer.parseInt(st.nextToken()); // 1 <= M <= 100
+		int m = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
 
-		// 출발: (0,0), 도착: (N-1, M-1)
-		char[][] board = new char[N][M];
-		int[][] visit = new int[N][M]; // 해당 방으로 이동하기 위해 벽을 최소 몇 개를 부숴야 하는지 저장
+		int[][] map = new int[n][m];
 
-		for (int i = 0; i < N; i++) {
-			String str = br.readLine();
-			for (int j = 0; j < M; j++) {
-				board[i][j] = str.charAt(j);
+		for (int i = 0; i < n; i++) {
+			String inputString = br.readLine();
+			for (int j = 0; j < m; j++) {
+				map[i][j] = inputString.charAt(j) - '0';
 			}
 		}
 
-		for (int i = 0; i < N; i++) {
-			Arrays.fill(visit[i], N * M);
+		// 0-1 BFS
+		int[][] dist = new int[n][m];
+		for (int i = 0; i < n; i++) {
+			Arrays.fill(dist[i], INF);
 		}
 
-		visit[0][0] = 0;
+		Deque<Room> dq = new ArrayDeque<>();
+		dist[0][0] = 0;
+		dq.offerFirst(new Room(0, 0, 0));
 
-		Deque<Room> q = new ArrayDeque<>();
-		q.offer(new Room(0, 0, 0));
-
-		while (!q.isEmpty()) {
-			Room cur = q.poll();
-
-			// 목적지 도착
-			if (cur.r == N - 1 && cur.c == M - 1) {
-				if (cur.cnt < visit[N - 1][M - 1]) {
-					visit[N - 1][M - 1] = cur.cnt;
-					break;
-				}
+		while (!dq.isEmpty()) {
+			Room cur = dq.pollFirst();
+			// 스테일 상태 스킵
+			if (cur.cnt != dist[cur.r][cur.c]) {
+				continue;
 			}
-
-			// board 4방향 탐색. 0이라면 현재 cnt 그대로, 1이라면 cnt+1
-			// visit 의 값과 cnt 값을 비교하여 이동
+			// 도착시 종료
+			if (cur.r == n - 1 && cur.c == m - 1) {
+				break;
+			}
+			// 탐색
 			for (int k = 0; k < 4; k++) {
 				int r = cur.r + dRow[k];
 				int c = cur.c + dCol[k];
-
-				if (r < 0 || c < 0 || r >= N || c >= M) {
+				if (r < 0 || r >= n || c < 0 || c >= m) {
 					continue;
 				}
-
-				if (board[r][c] == '0') {
-					if (cur.cnt >= visit[r][c]) {
-						continue;
+				int w = map[r][c];
+				int nd = cur.cnt + w;
+				if (dist[r][c] > nd) {
+					dist[r][c] = nd;
+					if (w == 0) {
+						dq.offerFirst(new Room(r, c, nd));
+					} else {
+						dq.offerLast(new Room(r, c, nd));
 					}
-					visit[r][c] = cur.cnt;
-					q.addFirst(new Room(r, c, cur.cnt));
-				} else {
-					if (cur.cnt + 1 >= visit[r][c]) {
-						continue;
-					}
-					visit[r][c] = cur.cnt + 1;
-					q.offer(new Room(r, c, cur.cnt + 1));
 				}
 			}
 		}
 
-		bw.write(visit[N - 1][M - 1] + "\n");
+		bw.write(dist[n - 1][m - 1] + "\n");
 		bw.flush();
 		bw.close();
 		br.close();
 	}
 
-	static class Room {
+	private static class Room {
 		int r;
 		int c;
 		int cnt;
