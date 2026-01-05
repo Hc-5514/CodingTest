17e89ee072d162a269ae1da13cffac7e4169ec5c
Hc-5514 <tbd05050@naver.com>
2026-01-05 18:02:28 +0900

    [BOJ] 1463번 : 1로 만들기

--- src/Baekjoon/AC/CLASS_3/BOJ_1463.java
+++ src/Baekjoon/AC/CLASS_3/BOJ_1463.java
@@ -1,54 +1,49 @@
 /**
- * 문제 : 1로 만들기
- *
- * @author Hc-5514
+ * 문제: 1로 만들기
+ * 난이도: 실버 3
+ * 메모리: 15836KB, 시간: 104ms
+ * 풀이: DP
  */
 
 package Baekjoon.AC.CLASS_3;
 
-import java.io.*;
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class BOJ_1463 {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	private static final int INF = 1_000_000_000;
 
-        int N = Integer.parseInt(br.readLine().trim());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int[] d = new int[N];
-        int cnt = 0;
+		// 3개의 연산을 사용해서 1을 만들 때, 연산 사용 횟수 최솟값 구하기
 
-        Queue<Integer> q = new ArrayDeque<>();
-        q.offer(N);
-        w1:
-        while (!q.isEmpty()) {
-            cnt++;
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                int n = q.poll();
-                if (n == 1) break w1;
+		int x = Integer.parseInt(br.readLine()); // 1 <= x <= 1_000_000
+		int[] dp = new int[x + 1];
 
-                if (n % 3 == 0 && d[n / 3] == 0) {
-                    d[n / 3] = cnt;
-                    q.offer(n / 3);
-                }
-                if (n % 2 == 0 && d[n / 2] == 0) {
-                    d[n / 2] = cnt;
-                    q.offer(n / 2);
-                }
-                if (d[n - 1] == 0) {
-                    d[n - 1] = cnt;
-                    q.offer(n - 1);
-                }
-            }
-        }
+		Arrays.fill(dp, INF);
+		dp[1] = 0;
 
-        bw.write(cnt - 1 + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+		// Bottom-up
+		for (int i = 2; i <= x; i++) {
+			dp[i] = Math.min(dp[i], dp[i - 1] + 1);
+			if (i % 2 == 0) {
+				dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+			}
+			if (i % 3 == 0) {
+				dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+			}
+		}
+
+		bw.write(dp[x] + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
 }
