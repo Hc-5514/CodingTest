1e1c8c554666447a43b8b3b11da90a7adc8e4ea6
Hc-5514 <tbd05050@naver.com>
2026-01-06 14:43:22 +0900

    [BOJ] 1932번 : 정수 삼각형

--- src/Baekjoon/AC/CLASS_4/BOJ_1932.java
+++ src/Baekjoon/AC/CLASS_4/BOJ_1932.java
@@ -1,54 +1,55 @@
 /**
- * 문제 : 정수 삼각형
- *
- * @author Hc-5514
+ * 문제: 정수 삼각형
+ * 난이도: 실버 1
+ * 메모리: 24884KB, 시간: 188ms
+ * 풀이: DP
  */
 
 package Baekjoon.AC.CLASS_4;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-/**
- * 알고리즘 분류: DP
- * 풀이 알고리즘: DP
- * [시간 복잡도]
- * O(N²)
- */
 public class BOJ_1932 {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
 
-        int n = Integer.parseInt(br.readLine().trim());
-        int[][] board = new int[n][n];
-        int[][] result = new int[n][n];
+		int n = Integer.parseInt(br.readLine()); // 1 <= n <= 500
+		int[][] triangle = new int[n][n];
+		int[][] dp = new int[n][n];
 
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j <= i; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j <= i; j++) {
+				triangle[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
 
-        result[0][0] = board[0][0];
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j <= i; j++) {
-                result[i + 1][j] = Math.max(result[i + 1][j], result[i][j] + board[i + 1][j]);
-                result[i + 1][j + 1] = Math.max(result[i + 1][j + 1], result[i][j] + board[i + 1][j + 1]);
-            }
-        }
+		dp[0][0] = triangle[0][0];
 
-        int max = 0;
-        for (int i = 0; i < n; i++) {
-            max = Math.max(max, result[n - 1][i]);
-        }
+		for (int i = 1; i < n; i++) {
+			dp[i][0] = dp[i - 1][0] + triangle[i][0];
+			dp[i][i] = dp[i - 1][i - 1] + triangle[i][i];
+			for (int j = 1; j < i; j++) {
+				dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
+			}
+		}
 
-        bw.write(max + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+		int result = Integer.MIN_VALUE;
+		for (int i = 0; i < n; i++) {
+			result = Math.max(result, dp[n - 1][i]);
+		}
+
+		bw.write(result + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
 }
