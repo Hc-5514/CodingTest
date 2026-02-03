3d280f774af8d2abd04ad8bb345067bde24d0e41
Hc-5514 <tbd05050@naver.com>
2026-02-04 06:57:20 +0900

    [BOJ] 1038번 : 감소하는 수

--- src/Baekjoon/Algorithm/BFSDFS/BOJ_1038.java
+++ src/Baekjoon/Algorithm/BFSDFS/BOJ_1038.java
@@ -1,7 +1,7 @@
 /**
  * 문제: 감소하는 수
  * 난이도: 골드 5
- * 메모리: 11780KB, 시간: 64ms
+ * 메모리: 11692KB, 시간: 68ms
  * 풀이: BFS
  */
 
@@ -13,8 +13,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Queue;
 
 public class BOJ_1038 {
@@ -24,30 +22,38 @@ public class BOJ_1038 {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N = Integer.parseInt(br.readLine().trim()); // 0 <= N <= 1_000_000
 
-		List<Long> nums = new ArrayList<>(); // 감소하는 수
+		// 감소하는 수 경우의 수 개수: 2^10 - 1 = 1023
+		if (N >= 1023) {
+			bw.write(String.valueOf(-1));
+			bw.flush();
+			return;
+		}
 
+		// 감소하는 수 생성
 		Queue<Long> q = new ArrayDeque<>();
 		for (long i = 0; i < 10; i++) {
-			nums.add(i);
 			q.offer(i);
 		}
 
+		int cnt = -1;
 		while (!q.isEmpty()) {
-			// cur 마지막 값보다 작은 수 추가
 			long cur = q.poll();
-			long last = cur % 10;
-			for (long i = 0; i < last; i++) {
-				long next = cur * 10 + i;
-				nums.add(next);
-				q.offer(next);
+			cnt++;
+
+			if (cnt == N) {
+				bw.write(String.valueOf(cur));
+				bw.flush();
+				return;
+			}
+
+			long lastDigit = cur % 10;
+			for (long digit = 0; digit < lastDigit; digit++) {
+				q.offer(cur * 10 + digit);
 			}
 		}
 
-		long result = N <= (nums.size() - 1) ? nums.get(N) : -1;
-
-		bw.write(String.valueOf(result));
+		// 안전 장치
+		bw.write(String.valueOf(-1));
 		bw.flush();
-		bw.close();
-		br.close();
 	}
 }
