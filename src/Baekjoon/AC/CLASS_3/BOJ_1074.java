bc0a96f2d735c3a7dd4dd6bf0ffdf099c4ede13f
Hc-5514 <tbd05050@naver.com>
2026-02-24 17:15:17 +0900

    [BOJ] 1074번 : Z

--- src/Baekjoon/AC/CLASS_3/BOJ_1074.java
+++ src/Baekjoon/AC/CLASS_3/BOJ_1074.java
@@ -1,62 +1,61 @@
 /**
- * 문제 : Z
- *
- * @author Hc-5514
+ * 문제: Z
+ * 난이도: 골드 5
+ * 메모리: 11628KB, 시간: 60ms
+ * 풀이: 분할 정복
  */
 
 package Baekjoon.AC.CLASS_3;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class BOJ_1074 {
 
-    static int n, r, c, cnt;
+	private static int r, c;
 
-    private static void division(int startR, int startC, int size) {
-        if (size == 2) {
-            if (r == startR) {
-                if (c == startC)
-                    cnt += 1;
-                else cnt += 2;
-            } else {
-                if (c == startC)
-                    cnt += 3;
-                else cnt += 4;
-            }
-            return;
-        }
+	private static int getOrder(int sR, int sC, int order, int len) {
+		if (r == sR && c == sC) {
+			return order;
+		}
 
-        // r,c의 위치 찾기
-        int half = size / 2;
-        if (r - startR < half && c - startC < half) {
-            division(startR, startC, half);
-        } else if (r - startR < half && c - startC >= half) {
-            cnt += (half * half);
-            division(startR, startC + half, half);
-        } else if (r - startR >= half && c - startC < half) {
-            cnt += (half * half * 2);
-            division(startR + half, startC, half);
-        } else {
-            cnt += (half * half * 3);
-            division(startR + half, startC + half, half);
-        }
-    }
+		int nxtlen = len / 2;
+		int hR = sR + nxtlen; // 행 절반 시작 위치
+		int hC = sC + nxtlen; // 열 절반 시작 위치
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+		int result = -1;
+		int area = (nxtlen) * (nxtlen);
 
-        n = Integer.parseInt(st.nextToken());
-        int size = (int) Math.pow(2, n);
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
+		if (r < hR && c < hC) {
+			result = getOrder(sR, sC, order, nxtlen);
+		} else if (r < hR && c >= hC) {
+			result = getOrder(sR, hC, order + area, nxtlen);
+		} else if (r >= hR && c < hC) {
+			result = getOrder(hR, sC, order + area * 2, nxtlen);
+		} else {
+			result = getOrder(hR, hC, order + area * 3, nxtlen);
+		}
 
-        division(0, 0, size);
+		return result;
+	}
 
-        System.out.println(cnt - 1);
-        br.close();
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken()); // 배열 4등분 횟수, 1 <= N <= 15
+		r = Integer.parseInt(st.nextToken()); // 행, 0 <= r <= 2^15
+		c = Integer.parseInt(st.nextToken()); // 열, 0 <= c <= 2^15
+
+		int len = (int)Math.pow(2, N);
+		int result = getOrder(0, 0, 0, len);
+
+		bw.write(String.valueOf(result));
+		bw.flush();
+	}
 }
