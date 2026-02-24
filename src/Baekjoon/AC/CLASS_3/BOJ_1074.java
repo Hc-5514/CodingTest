63ad11b82aeef8225f458dafa77d38ffedbdb294
Hc-5514 <tbd05050@naver.com>
2026-02-24 17:18:53 +0900

    [BOJ] 1074번 : Z

--- src/Baekjoon/AC/CLASS_3/BOJ_1074.java
+++ src/Baekjoon/AC/CLASS_3/BOJ_1074.java
@@ -1,7 +1,7 @@
 /**
  * 문제: Z
  * 난이도: 골드 5
- * 메모리: 11628KB, 시간: 60ms
+ * 메모리: 11616KB, 시간: 60ms
  * 풀이: 분할 정복
  */
 
@@ -19,7 +19,7 @@ public class BOJ_1074 {
 	private static int r, c;
 
 	private static int getOrder(int sR, int sC, int order, int len) {
-		if (r == sR && c == sC) {
+		if (len == 1) {
 			return order;
 		}
 
@@ -27,20 +27,17 @@ public class BOJ_1074 {
 		int hR = sR + nxtlen; // 행 절반 시작 위치
 		int hC = sC + nxtlen; // 열 절반 시작 위치
 
-		int result = -1;
 		int area = (nxtlen) * (nxtlen);
 
 		if (r < hR && c < hC) {
-			result = getOrder(sR, sC, order, nxtlen);
+			return getOrder(sR, sC, order, nxtlen);
 		} else if (r < hR && c >= hC) {
-			result = getOrder(sR, hC, order + area, nxtlen);
+			return getOrder(sR, hC, order + area, nxtlen);
 		} else if (r >= hR && c < hC) {
-			result = getOrder(hR, sC, order + area * 2, nxtlen);
+			return getOrder(hR, sC, order + area * 2, nxtlen);
 		} else {
-			result = getOrder(hR, hC, order + area * 3, nxtlen);
+			return getOrder(hR, hC, order + area * 3, nxtlen);
 		}
-
-		return result;
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -52,7 +49,7 @@ public class BOJ_1074 {
 		r = Integer.parseInt(st.nextToken()); // 행, 0 <= r <= 2^15
 		c = Integer.parseInt(st.nextToken()); // 열, 0 <= c <= 2^15
 
-		int len = (int)Math.pow(2, N);
+		int len = 1 << N;
 		int result = getOrder(0, 0, 0, len);
 
 		bw.write(String.valueOf(result));
